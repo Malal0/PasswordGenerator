b6d3db725ce4bba18af2b2fca9d2e9f09a1622a1
UncomfortableChai <mj1055119@gmail.com>
2022-06-28 13:36:09 -0700

    added character length input

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
     "/"];
 
-let firstPassword = "";
-let secondPassword = "";
-let passwordLength = 15;
-
 const firstPasswordText = document.getElementById("first-password");
 const secondPasswordText = document.getElementById("second-password");
-const firstPasswordBtn = document.querySelector("#first-password-btn");
-const secondPasswordBtn = document.querySelector("#second-password-btn");
+const characterLength = document.querySelector("#number-of-characters");
+
+let firstPassword = "";
+let secondPassword = "";
+let passwordLength = 8;
 
 function random() {
     let output = ""
@@ -19,21 +18,22 @@ function random() {
     return output;
 };
 
+function changeCharacterLength() {
+    passwordLength = this.value;
+}
+
 function generatePasswords() {
-    /*firstPassword = "";
-    secondPassword = "";*/
     firstPassword = random();
     secondPassword = random();
     firstPasswordText.textContent = firstPassword;
     secondPasswordText.textContent = secondPassword;
-    firstPasswordBtn.value = firstPassword;
-    secondPasswordBtn.value = secondPassword;
 }
 
 function copyToClipboard() {
-    console.log(this.value);
-    navigator.clipboard.writeText(this.value);
+    console.log(this.innerText);
+    navigator.clipboard.writeText(this.innerText);
 }
 
-firstPasswordBtn.addEventListener("click", copyToClipboard);
-secondPasswordBtn.addEventListener("click", copyToClipboard);
+characterLength.addEventListener("change", changeCharacterLength);
+firstPasswordText.addEventListener("click", copyToClipboard);
+secondPasswordText.addEventListener("click", copyToClipboard);
